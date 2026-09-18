d286298157378fa4e20058029242400197b55872
janani <jananiyadlapalli@gmail.com>
2026-09-18 11:37:41 +0530

    Add files via upload

---
+++ LAC_CODE_04.c
@@ -0,0 +1,14 @@
+// reverse the array
+#include<stdio.h>
+int main(){
+    int arr[]={9,18,27,36,45};
+    int n=5;
+    for(int i=n-1;i>=0;i--){
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
+
+
+
