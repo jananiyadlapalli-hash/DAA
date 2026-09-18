9005e0a92465abe4c83970218aa9d281ac4b0a7c
janani <jananiyadlapalli@gmail.com>
2026-09-18 10:29:44 +0530

    Add files via upload

---
+++ LAB_CODE_01.c
@@ -0,0 +1,20 @@
+//write and display all the elements of an array.
+#include<stdio.h>
+int main(){
+    int n = 6;
+    int arr[]={7,14,21,28,35,42};
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
